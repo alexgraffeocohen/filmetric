5f122d5b33ea288b6c79e15782b83df45b7090e0
alexwilkinson <alex.wilkinson@me.com>
2014-03-16 21:46:56 -0400

    added alerts

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,6 +4,10 @@ class SearchesController < ApplicationController
   end
 
   def show
+    if params[:q].empty?
+      flash.now[:notice] = "You gotta give me something! : )"
+      render "search"
+    end
     case params[:category]
       when "Movie"
         @results = Movie.where('title LIKE ?', "%#{params[:q]}%")
@@ -13,6 +17,10 @@ class SearchesController < ApplicationController
         @results = Director.where('name LIKE ?', "%#{params[:q]}%")
       when "Genre"
         @results = Genre.where('name LIKE ?', "%#{params[:q]}%")
+    end
+    if @results.empty?
+      flash.now[:notice] = "I'm sorry, nothing matched what you were looking for."
+      render "search" 
     end
   end
 
