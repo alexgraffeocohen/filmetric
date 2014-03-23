46394f5e37bee7087408b55f60e6753b96a49c19
alexwilkinson <alex.wilkinson@me.com>
2014-03-22 20:40:23 -0400

    case insensitive

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,18 +9,20 @@ class SearchesController < ApplicationController
       render "search"
     end
 
+    query = params[:q].split.collect(&:capitalize).join(" ")
+
     case params[:category]
       when "Movie"
-        @results = Movie.where('title LIKE ?', "%#{params[:q]}%")
+        @results = Movie.where('title LIKE ?', "%#{query}%")
         redirect_to movie_path(@results.first) if @results.count == 1
       when "Actor"
-        @results = Actor.where('name LIKE ?', "%#{params[:q]}%")
+        @results = Actor.where('name LIKE ?', "%#{query}%")
         redirect_to actor_path(@results.first) if @results.count == 1
       when "Director"
-        @results = Director.where('name LIKE ?', "%#{params[:q]}%")
+        @results = Director.where('name LIKE ?', "%#{query}%")
         redirect_to director_path(@results.first) if @results.count == 1
       when "Genre"
-        @results = Genre.where('name LIKE ?', "%#{params[:q]}%")
+        @results = Genre.where('name LIKE ?', "%#{query}%")
         redirect_to genre_path(@results.first) if @results.count == 1
     end
     
