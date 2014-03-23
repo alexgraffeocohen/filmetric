92107a4553a8123c08b0688521723d12457aa975
alexwilkinson <alex.wilkinson@me.com>
2014-03-23 12:08:14 -0400

    fixed styles, case-insensitive search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class SearchesController < ApplicationController
 
   def search
@@ -9,20 +11,20 @@ class SearchesController < ApplicationController
       render "search"
     end
 
-    query = params[:q].split.collect(&:capitalize).join(" ")
+    query = params[:q].split.collect(&:downcase).join(" ")
 
     case params[:category]
       when "Movie"
-        @results = Movie.where('title LIKE ?', "%#{query}%")
+        @results = Movie.where('lower(title) LIKE ?', "%#{query}%")
         redirect_to movie_path(@results.first) if @results.count == 1
       when "Actor"
-        @results = Actor.where('name LIKE ?', "%#{query}%")
+        @results = Actor.where('lower(name) LIKE ?', "%#{query}%")
         redirect_to actor_path(@results.first) if @results.count == 1
       when "Director"
-        @results = Director.where('name LIKE ?', "%#{query}%")
+        @results = Director.where('lower(name) LIKE ?', "%#{query}%")
         redirect_to director_path(@results.first) if @results.count == 1
       when "Genre"
-        @results = Genre.where('name LIKE ?', "%#{query}%")
+        @results = Genre.where('lower(name) LIKE ?', "%#{query}%")
         redirect_to genre_path(@results.first) if @results.count == 1
     end
     
