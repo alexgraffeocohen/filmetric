7151730991befbb86c5f394e4473366b98b21ad6
alexwilkinson <alex.wilkinson@me.com>
2014-04-30 21:02:13 -0400

    extracted checking_rt and saving and returning result logic from searches#show

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,28 +6,20 @@ class SearchesController < ApplicationController
   def show
     if params[:q].empty?
       flash.now[:notice] = "You gotta give me something!"
-      render "search"
+      return render "search"
     end
 
     query = params[:q].split.collect(&:downcase).join(" ")
-
     check_for_query_category(params[:category], query)
     
     if @results.empty?
-      case params[:category]
-      when "Movie"
-        queried_result = RTQuerier.find_by_title(query)
-        if queried_result.empty?
-          flash.now[:notice] = "I'm sorry, nothing matched what you were looking for."
-          render "search"
-        end
-        RTQuerier.save_to_db(queried_result)
-        @results = Movie.where('lower(title) LIKE ?', "%#{query}%")
-        redirect_to movie_path(@results.first) if @results.count == 1
-        return
-      end
-      flash.now[:notice] = "I'm sorry, nothing matched what you were looking for."
-      render "search" 
+      rt_result = check_rt(params[:category], query)
+      if rt_result.empty?
+        flash.now[:notice] = "I'm sorry, nothing matched what you were looking for."
+        return render "search"
+      else
+        save_and_return(rt_result, query)
+      end 
     end
   end
 
@@ -50,4 +42,21 @@ class SearchesController < ApplicationController
     end
   end
 
+  def check_rt(category, query)
+    case category
+    when "Movie"
+      RTQuerier.find_by_title(query)
+    end
+  end
+  
+  def save_and_return(rt_result, query)
+    begin
+      RTQuerier.save_to_db(rt_result)
+      @results = Movie.where('lower(title) LIKE ?', "%#{query}%")
+      return redirect_to movie_path(@results.first) if @results.count == 1
+    rescue
+      flash.now[:notice] = "I'm sorry, nothing matched what you were looking for."
+      return render "search"
+    end
+  end
 end
