c7713f17489ca1d37006969fbd0a998c2a2517f0
alexwilkinson <alex.wilkinson@me.com>
2014-05-01 13:19:31 -0400

    fixing searching feature spec, deleted uneeded route;

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 
-describe 'searching' do
-  before(:all) do
-    Movie.destroy_all
-
+feature 'Search' do
+  before(:each) do
     Movie.create(
                    :id => 1798709,
                 :title => "Her",
-         :release_date => "2013",
+         :release_date => "Fri, 10 Jan 2014",
         :critics_score => 94,
        :audience_score => 85,
     :critics_consensus => "Sweet, soulful, and smart, Spike Jonze's Her uses its just-barely-sci-fi scenario to impart wryly funny wisdom about the state of modern human relationships.",
@@ -20,7 +18,7 @@ describe 'searching' do
     Movie.create(
                    :id => 1454468,
                 :title => "Gravity",
-         :release_date => "2013",
+         :release_date => "Fri, 04 Oct 2013",
         :critics_score => 97,
        :audience_score => 84,
     :critics_consensus => "Alfonso Cuaron's Gravity is an eerie, tense sci-fi thriller that's masterfully directed and visually stunning.",
@@ -33,7 +31,7 @@ describe 'searching' do
     Movie.create(
                    :id => 206634,
                 :title => "Children of Men",
-         :release_date => "2006",
+         :release_date => "Mon, 25 Dec 2006",
         :critics_score => 93,
        :audience_score => 84,
     :critics_consensus => "Children of Men works on every level: as a violent chase thriller, a fantastical cautionary tale, and a sophisticated human drama about societies struggling to live. This taut and thought-provoking tale may not have the showy special effects normally found in movies of this genre, but you won't care one bit after the story kicks in, about a dystopic future where women can no longer conceive and hope lies within one woman who holds the key to humanity's survival. It will have you riveted.",
@@ -45,7 +43,7 @@ describe 'searching' do
     Movie.create(
                    :id => 95016,
                 :title => "Die Hard",
-         :release_date => "1988",
+         :release_date => "Fri, 15 Jul 1988",
         :critics_score => 92,
        :audience_score => 94,
     :critics_consensus => "Its many imitators (and sequels) have never come close to matching the taut thrills of the definitive holiday action classic.",
@@ -54,10 +52,17 @@ describe 'searching' do
               :rt_link => "http://www.rottentomatoes.com/m/die_hard/",
             :filmetric => -2
       )
+     Movie.create(
+                   :id => 112864,
+                :title => "Die Hard 3: With a Vengeance",
+         :release_date => "Fri, 19 May 1995",
+        :critics_score => 51,
+       :audience_score => 84
+      )
     Movie.create(
                    :id => 499549,
                 :title => "Avatar",
-         :release_date => "2009",
+         :release_date => "Fri, 18 Dec 2009",
         :critics_score => 83,
        :audience_score => 82,
     :critics_consensus => "It might be more impressive on a technical level than as a piece of storytelling, but Avatar reaffirms James Cameron's singular gift for imaginative, absorbing filmmaking.",
@@ -69,7 +74,7 @@ describe 'searching' do
     Movie.create(
                    :id => 87332,
                 :title => "Ghostbusters",
-         :release_date => "1984",
+         :release_date => "Fri, 01 Jun 1984",
         :critics_score => 96,
        :audience_score => 87,
     :critics_consensus => "An infectiously fun blend of special effects and comedy, with Bill Murray's hilarious deadpan performance leading a cast of great comic turns.",
@@ -78,101 +83,67 @@ describe 'searching' do
               :rt_link => "http://www.rottentomatoes.com/m/ghostbusters/",
             :filmetric => 9
       )
+    
+    Actor.create(name: "Brad Pitt", filmetric: -5)
+    Director.create(name: "Michael Bay", filmetric: -28)
+    Genre.create(name: "Action", filmetric: -5)
   end
 
-  describe 'executing a movie search' do 
-    context 'on the search page' do
-      before do
-        visit search_path
-      end
+  scenario 'executing a movie search' do   
+    visit search_path
+    select('Movie', :from => 'category')
+    fill_in 'q', with: 'Gravity'
+    click_button('Search')
 
-      it 'should have a search form' do
-        expect(page).to have_css('form')
-      end
-      
-      it 'should execute a new search' do
-        select('Movie', :from => 'category')
-        fill_in 'q', with: 'Gravity'
-        click_button('Search')
-
-        expect(page).to have_content("Gravity")
-      end
-    end
+    expect(page).to have_content("Gravity")
   end
 
-  describe 'executing a search for a movie not in db' do
-    before do
-      visit search_path
-    end
+  scenario 'executing a movie search with multiple results' do
+    visit search_path
+    select('Movie', :from => 'category')
+    fill_in 'q', with: 'Die Hard'
+    click_button('Search')
 
-    context 'if only one movie matches query' do
-      it 'should query for movie and display it' do
-        select('Movie', :from => 'category')
-        fill_in 'q', with: 'The Life Aquatic with Steve Zissou'
-        click_button('Search')
+    expect(page).to have_content("Die Hard")
+    expect(page).to have_content("Die Hard 3: With a Vengeance")
+  end
 
-        expect(page).to have_content("The Life Aquatic with Steve Zissou")
-      end 
-    end
+  scenario 'querying for movie not in database' do
+    visit search_path
+    select('Movie', :from => 'category')
+    fill_in 'q', with: 'The Iron Giant'
+    click_button('Search')
+
+    expect(page).to have_content("The Iron Giant")
   end 
+   
+  scenario 'searching for an actor in database' do
+    visit search_path
+    select('Actor', :from => 'category')
+    fill_in 'q', with: 'Brad'
+    click_button('Search')
 
-  describe 'executing an actor search' do
-    before do
-      Actor.create(name: "Brad Pitt", filmetric: -5)
-    end
-
-    context 'on the search page' do
-      before do
-        visit search_path
-      end
-
-      it 'should correctly find an actor' do
-        select('Actor', :from => 'category')
-        fill_in 'q', with: 'Brad'
-        click_button('Search')
-
-        expect(page).to have_content("Brad Pitt")
-      end
-    end
+    expect(page).to have_content("Brad Pitt")
   end
+    
 
-  describe 'executing a director search' do
-    before do
-      Director.create(name: "Michael Bay", filmetric: -28)
-    end
+  scenario 'searching for a director in database' do
+    visit search_path
+    select('Director', :from => 'category')
+    fill_in 'q', with: 'Michael Bay'
+    click_button('Search')
 
-    context 'on the search page' do
-      before do
-        visit search_path
-      end
-
-      it 'should correctly find an actor' do
-        select('Director', :from => 'category')
-        fill_in 'q', with: 'Michael Bay'
-        click_button('Search')
-
-        expect(page).to have_content("Michael Bay")
-      end
-    end
+    expect(page).to have_content("Michael Bay")
   end
+   
 
-  describe 'executing a genre search' do
-    before do
-      Genre.create(name: "Action", filmetric: -5)
-    end
+  scenario 'searching for a genre in database' do
+    visit search_path
+    select('Genre', :from => 'category')
+    fill_in 'q', with: 'Action'
+    click_button('Search')
 
-    context 'on the search page' do
-      before do
-        visit search_path
-      end
-
-      it 'should correctly find an actor' do
-        select('Genre', :from => 'category')
-        fill_in 'q', with: 'Action'
-        click_button('Search')
-
-        expect(page).to have_content("Action")
-      end
-    end
+    expect(page).to have_content("Action")
   end
 end
+   
