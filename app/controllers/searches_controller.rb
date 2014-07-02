fe3c6a294ec10e67725f4ce83c75f3d0aa79b452
alexwilkinson <alex.wilkinson@me.com>
2014-07-02 09:27:29 -0400

    implemented json endpoint for search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,29 +4,39 @@ class SearchesController < ApplicationController
   end
 
   def show
-    if params[:q].empty?
-      flash.now[:notice] = "You gotta give me something!"
-      return render "search"
-    end
+    respond_to do |f|
+      if params[:q].empty?
+        flash.now[:notice] = "You gotta give me something!"
+        f.html { return render "search" }
+      end
 
-    query = params[:q].split.collect(&:downcase).join(" ")
-    handle_query(params[:category], query)
+      query = params[:q].split.collect(&:downcase).join(" ")
+      handle_query(params[:category], query, f)
+    end
   end
 
   private
 
-  def handle_query(category, query)
-    @results = check_database(category, query)
-    if @results.blank?
-      @results = check_rt(category, query)
-      if @results.blank?
+  def handle_query(category, query, f)
+    results_from_db = check_database(category, query)
+    if results_from_db.blank?
+      results_from_rt = check_rt(category, query)
+      if results_from_rt.blank?
         flash.now[:notice] = "I'm sorry, nothing matched what you were looking for."
-        return render "search"
+        f.html { render "search" }
+        f.json { render json: {}, status: 200 }
       else
-        return save_and_display(@results, query)
+        return save_and_display(results_from_rt, query, f)
       end
     end
-    redirect_to send("#{category.downcase}_path", @results.first) if @results.count == 1
+
+    @results = results_from_db
+    f.json { render json: @results, status: 200 }
+    if @results.count == 1
+      f.html { redirect_to send("#{category.downcase}_path", results_from_db.first) }
+    else
+      f.html { render "show" }
+    end
   end
 
   def check_database(category, query)
@@ -44,13 +54,15 @@ class SearchesController < ApplicationController
     end
   end
 
-  def save_and_display(rt_result, query)
+  def save_and_display(rt_result, query, f)
     RTQuerier.save_to_db(rt_result)
     @results = Movie.where('lower(title) LIKE ?', "%#{query}%")
     if @results.empty?
       flash.now[:notice] = "I'm sorry, nothing matched what you were looking for."
-      return render "search"
+      f.html { return render "search" }
+      f.json { render json: {}, status: 200 }
     end
-    redirect_to movie_path(@results.first) if @results.count == 1
+    f.html { redirect_to movie_path(@results.first) if @results.count == 1 }
+    f.json { render json: @results, status: 200 }
   end
 end
