f9d5b34716a3eb060b819add8fe1c2452beff95e
alexwilkinson <alex.wilkinson@me.com>
2014-08-24 16:18:35 -0400

    disabed transactional fixtures so that data is properly loaded in js feature tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,13 +35,10 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -57,19 +54,19 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
- 
+
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
- 
+
   config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
- 
+
   config.before(:each) do
     DatabaseCleaner.start
   end
- 
+
   config.after(:each) do
     DatabaseCleaner.clean
   end
