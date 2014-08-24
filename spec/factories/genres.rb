b7e83506c0979ecb085cd80e8c042a0bfcc967f1
alexwilkinson <alex.wilkinson@me.com>
2014-08-24 16:14:46 -0400

    adjusted factories so they are more useful in specs

--- spec/factories/genres.rb
+++ spec/factories/genres.rb
@@ -2,6 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :genre do
-    name { Faker::Name.name }
+    name "Action"
+    filmetric { -5 }
   end
 end
--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -9,7 +9,12 @@ FactoryGirl.define do
     audience_score { Faker::Number.number(2).to_i }
     filmetric { critics_score - audience_score }
     critics_consensus { Faker::Lorem.sentence }
-    poster_link { 'http://content9.flixster.com/movie/11/17/28/11172839_ori.jpg' }
+    poster_link { "http://content6.flixster.com/movie/11/16/80/11168096_ori.jpg" }
     rating { 'R' }
+
+    factory :movie_critics_like_more do
+      critics_score 95
+      audience_score 90
+    end
   end
 end
